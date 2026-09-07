4367203818ca9067592dbf764efa80bcd221cc5f
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-09-07 08:33:25 +0000

    feat: add Express routes and static files

---
+++ index.js
@@ -0,0 +1,19 @@
+const path = require("node:path");
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
